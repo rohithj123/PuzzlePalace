96f50c92a4e2b36ef86e3a9201da2fbb1ca1f928
Mehul Bellukutty <mehulbrainy@gmail.com>
2025-10-03 21:18:26 -0400

    fixed json.simple error.

--- puzzlepalace/src/main/java/module-info.java
+++ puzzlepalace/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.puzzlepalace {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.puzzlepalace to javafx.fxml;
-    exports com.puzzlepalace;
+    exports com.example;
 }

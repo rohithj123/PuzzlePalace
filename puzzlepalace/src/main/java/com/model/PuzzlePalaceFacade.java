0e4332289ee3b71bd68f043d6640d0f495452f01
Pranav Kanumuri <pranavkk@email.sc.edu>
2025-10-05 20:42:53 -0400

    PuzzlePalaceFacade stub #16

---
+++ puzzlepalace/src/main/java/com/model/PuzzlePalaceFacade.java
@@ -0,0 +1,78 @@
+package com.model;
+
+import java.util.List;
+
+public class PuzzlePalaceFacade {
+
+    private Player currentPlayer;
+    private Progress progress;
+    private Leaderboard leaderboard;
+    private Settings settings;
+
+    public Player login(String userName, String password) {
+        return null;
+    }
+
+    public void logout() {
+    }
+
+    public Player createAccount(String userName, String password) {
+        return null;
+    }
+
+    public void deleteAccount(int playerId) {
+    }
+
+    public void startNewGame() {
+    }
+
+    public void continueGame() {
+    }
+
+    public String showInstructions() {
+        return null;
+    }
+
+    public void updateSettings(Settings settings) {
+    }
+
+    public Settings getSettings() {
+        return settings;
+    }
+
+    public void toggleSound(boolean on) {
+    }
+
+    public void setLanguage(String langCode) {
+    }
+
+    public void setDifficulty(String level) {
+    }
+
+    public void enterRoom(int roomId) {
+    }
+
+    public Room getCurrentRoom() {
+        return null;
+    }
+
+    public List<Room> listAvailableRooms() {
+        return null;
+    }
+
+    public Puzzle getPuzzle(int puzzleId) {
+        return null;
+    }
+
+    public boolean submitPuzzleAnswer(int puzzleId, String answer) {
+        return false;
+    }
+
+    public List<Clue> getCluesForPuzzle(int puzzleId) {
+        return null;
+    }
+
+    public boolean useItem(int itemId, int targetId) {
+        return false;
+    }
+}

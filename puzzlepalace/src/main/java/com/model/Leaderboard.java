44a858c4847ccb3bd5082e01052b55d0f1f77d47
Pranav Kanumuri <pranavkk@email.sc.edu>
2025-10-05 20:04:21 -0400

    Leaderboard Class Stub #13

---
+++ puzzlepalace/src/main/java/com/model/Leaderboard.java
@@ -0,0 +1,19 @@
+package com.model;
+import java.util.List;
+
+
+public class Leaderboard {
+
+    private List<Certificate> entries;
+    private List<Player> users;
+
+    public void addEntry() { }
+
+    public List displayTopScores() { 
+        return null; 
+    }
+
+    public int findUserRank() { 
+        return 0; 
+    }
+}

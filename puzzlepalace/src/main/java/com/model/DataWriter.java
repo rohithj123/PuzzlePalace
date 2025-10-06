4a14793f1ff20fbe3b20b9e6d08c3d82b6454266
Mehul Bellukutty <mehulbrainy@gmail.com>
2025-10-05 22:26:24 -0400

    Issue #55: Added DataWriter

---
+++ puzzlepalace/src/main/java/com/model/DataWriter.java
@@ -0,0 +1,89 @@
+package com.model;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class DataWriter {
+
+    private final String filePath;
+
+    public DataWriter(String filePath) 
+    {
+        this.filePath = filePath;
+    }
+
+    public void saveUsers(List<Player> players) 
+    {
+        JSONArray playerArray = new JSONArray();
+
+        for (Player p : players) {
+            String username = safeGetString(p, "getUsername", "username");
+            String email = safeGetString(p, "getEmail", "email");
+
+            JSONObject playerObj = new JSONObject();
+            playerObj.put("username", username);
+            playerObj.put("email", email);
+            playerArray.add(playerObj);
+        }
+
+        File file = new File(filePath);
+        File parent = file.getParentFile();
+        if (parent != null && !parent.exists())
+        {
+            parent.mkdirs();
+        }
+
+        try (FileWriter writer = new FileWriter(file)) 
+        {
+            writer.write(playerArray.toJSONString());
+            writer.flush();
+            System.out.println("DataWriter: wrote " + players.size() + " players to " + filePath);
+        } catch (IOException e) {
+            System.out.println("DataWriter: IO error while writing file: " + e.getMessage());
+        }
+    }
+
+    private String safeGetString(Object obj, String methodName, String fieldName) 
+    {
+        if (obj == null) return "";
+
+        try 
+        {
+            Method m = obj.getClass().getMethod(methodName);
+            Object val = m.invoke(obj);
+            return val == null ? "" : val.toString();
+        } catch (NoSuchMethodException ignored) 
+        {
+        } catch (Exception e)
+        {
+        }
+
+        try 
+        {
+            Field f = obj.getClass().getDeclaredField(fieldName);
+            f.setAccessible(true);
+            Object val = f.get(obj);
+            return val == null ? "" : val.toString();
+        } catch (NoSuchFieldException nsf) 
+        {
+            try 
+            {
+                Field alt = obj.getClass().getDeclaredField("_" + fieldName);
+                alt.setAccessible(true);
+                Object val = alt.get(obj);
+                return val == null ? "" : val.toString();
+            } catch (Exception ignored) {}
+        } catch (Exception e) 
+        {
+        }
+
+        return "";
+    }
+}

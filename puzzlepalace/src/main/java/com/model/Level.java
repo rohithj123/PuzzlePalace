eb3e7c2b375afbfc9893047d69246c1ea07e6f00
Pranav Kanumuri <pranavkk@email.sc.edu>
2025-10-05 20:52:06 -0400

    Level stub #25

---
+++ puzzlepalace/src/main/java/com/model/Level.java
@@ -0,0 +1,18 @@
+package com.model;
+
+public class Level {
+
+    private int levelNumber;
+    private String levelName;
+    private String difficulty;
+
+    public void loadLevel() { }
+
+    public void startLevel() { }
+
+    public void completeLevel() { }
+
+    public boolean isCompleted() { 
+        return false; 
+    }
+}

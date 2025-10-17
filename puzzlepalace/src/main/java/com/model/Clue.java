b056eae808b398b3bdbde113c10b8404bcff473e
Rohith Joseph <rohithjoseph12345@gmail.com>
2025-10-17 18:23:15 -0400

    fixed #66

--- puzzlepalace/src/main/java/com/model/Clue.java
+++ puzzlepalace/src/main/java/com/model/Clue.java
@@ -1,5 +1,29 @@
 package com.model;
 
 public class Clue {
+    private final int id;
+    private final String text;
+    private boolean revealed;
 
+    public Clue(int id, String text) {
+        this.id = id;
+        this.text = text == null ? "" : text;
+        this.revealed = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void reveal() {
+        this.revealed = true;
+    }
 }

6dc1f4e01758e3282582dd1ac8e191ae9f4c4c4d
Mehul Bellukutty <mehulbrainy@gmail.com>
2025-10-26 17:51:16 -0400

    Java Doc on Level.java

--- puzzlepalace/src/main/java/com/model/Level.java
+++ puzzlepalace/src/main/java/com/model/Level.java
@@ -1,4 +1,8 @@
 package com.model;
+/**
+ * This represents a level in the game.
+ * This stores the level number, name, and difficulty.
+ */
 
 public class Level {
 
@@ -6,12 +10,26 @@ public class Level {
     private String levelName;
     private String difficulty;
 
+    /**
+     * This loads the level data.
+     */
     public void loadLevel() { }
 
+     /**
+     * This starts the level.
+     */
     public void startLevel() { }
 
+    /**
+     * This marks the level as completed.
+     */
     public void completeLevel() { }
 
+    /**
+     * This checks if the level is completed.
+     *
+     * @return true if completed, false otherwise
+     */
     public boolean isCompleted() { 
         return false; 
     }

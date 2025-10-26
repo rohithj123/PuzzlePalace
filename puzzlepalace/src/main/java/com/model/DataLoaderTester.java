e863b20e1483ea64cca2f0278fefbcd3fb0bc184
Mehul Bellukutty <mehulbrainy@gmail.com>
2025-10-26 16:57:45 -0400

    Java Doc on DataLoaderTester.java

--- puzzlepalace/src/main/java/com/model/DataLoaderTester.java
+++ puzzlepalace/src/main/java/com/model/DataLoaderTester.java
@@ -2,7 +2,18 @@ package com.model;
 
 import java.util.List;
 
+/**
+ * This tests the DataLoader class.
+ * This loads player data from a JSON file and prints the results.
+ */
+
 public class DataLoaderTester {
+    /**
+     * This runs the test for DataLoader.
+     * This loads players from a JSON file and displays them in the console.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         String filePath = "data/users.json"; 
         List<Player> players = DataLoader.loadUsers(filePath);

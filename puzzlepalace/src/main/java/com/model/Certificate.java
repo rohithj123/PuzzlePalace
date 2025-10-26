ae326e0c39dfc9298e9febef769ffd158b027ba9
Mehul Bellukutty <mehulbrainy@gmail.com>
2025-10-26 16:44:45 -0400

    JavaDoc Certificate.Java

--- puzzlepalace/src/main/java/com/model/Certificate.java
+++ puzzlepalace/src/main/java/com/model/Certificate.java
@@ -1,4 +1,9 @@
 package com.model;
+/**
+ * This will show a player's achievement certificate in the game.
+ * This will store information about the certificate, player details,
+ * completion date, and score summary.
+ */
 
 public class Certificate {
     private String certificateId;
@@ -6,7 +11,19 @@ public class Certificate {
     private int completionDate;
     private String scoreSummary;
 
+    /**
+     * Default constructor.
+     */
+
     public Certificate() {
+    /**
+     * Constructor with parameters.
+     *
+     * @param certificateId is the ID of the certificate
+     * @param playerName is the player's name
+     * @param completionDate is the date the player completed the puzzle
+     * @param scoreSummary is the score or summary of performance
+     */
 
     }
 
@@ -16,7 +33,9 @@ public class Certificate {
         this.completionDate = completionDate;
         this.scoreSummary = scoreSummary;
     }
-
+    /**
+     * Generates a PDF for the certificate.
+     */
     public void generatePDF() {
         System.out.println("Generating PDF for certificate: " + certificateId);
     }
@@ -25,6 +44,9 @@ public class Certificate {
         System.out.println("emailCertificate() called with email: " + email);
     }
 
+    /**
+     * Displays the certificate details.
+     */
     public void displayCertificate() {
         System.out.println("displayCertificate() called (stub)");
     }
